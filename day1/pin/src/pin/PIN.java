301a48abb3e7823e4322d57d4b6434f2843431c1
Jacky <newbdez33@gmail.com>
2017-11-15 16:03:53 +0900

    用String替换int类型

--- day1/pin/src/pin/PIN.java
+++ day1/pin/src/pin/PIN.java
@@ -5,22 +5,19 @@ public class PIN {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		int pin = 12345;
+		String pin = "12345";
 
 		System.out.println("WELCOME TO THE BANK OF MITCHELL.");
 		System.out.print("ENTER YOUR PIN: ");
-		try {
-			int entry = keyboard.nextInt();
-			while ( entry != pin )
-			{
-				System.out.println("\nINCORRECT PIN. TRY AGAIN.");
-				System.out.print("ENTER YOUR PIN: ");
-				entry = keyboard.nextInt();
-			}
-			System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
-		}catch(Exception e) {
-			System.out.println("\nERROR.");
+		
+		String entry = keyboard.next();
+		while ( !entry.equals(pin) )
+		{
+			System.out.println("\nINCORRECT PIN. TRY AGAIN.");
+			System.out.print("ENTER YOUR PIN: ");
+			entry = keyboard.next();
 		}
+		System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
 
 	}
 
